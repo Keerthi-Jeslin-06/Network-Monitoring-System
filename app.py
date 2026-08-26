103d382236f5418763f8796955b170412871d364
keerthi-jeslin-06 <keerthijeslin7613@gmail.com>
2026-08-26 19:07:55 +0530

    Update Flask app for deployment

--- app.py
+++ app.py
@@ -817,11 +817,13 @@ def download_report():
 # START APPLICATION
 # ==========================================
 
-init_database()
-
-
 if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
+        host="0.0.0.0",
+        port=port,
         debug=True
     )
